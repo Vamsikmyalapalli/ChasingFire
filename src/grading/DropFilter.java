feb936b0a0f4e435c53423c7eb842b562f4fe999
Vamsi Krishna Myalapalli <vamsikmyalapalli@csus.edu>
2019-03-06 23:50:52 -0800

    Add files via upload

--- src/grading/DropFilter.java
+++ src/grading/DropFilter.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class DropFilter implements Filter {
 	private boolean shouldDropHighest, shouldDropLowest;
 
+	// Default constructor
 	public DropFilter() {
 		this(true, true);
 	}
