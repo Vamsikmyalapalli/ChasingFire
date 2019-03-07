008e73b89393c52162bee3e1525dce67e2037726
Vamsi Krishna Myalapalli <vamsikmyalapalli@csus.edu>
2019-03-06 23:53:09 -0800

    Delete GradingStrategy.java

--- GradingStrategy.java
+++
@@ -1,9 +0,0 @@
-package grading;
-
-import java.util.List;
-
-public interface GradingStrategy {
-    // Abstract method
-	public abstract Grade calculate(String key, List<Grade> grades) throws SizeException;
-
-}
