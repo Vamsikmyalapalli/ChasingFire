ec42ea6fb13336fd0c46d3001e5a841bf59dee1c
Vamsi Krishna Myalapalli <vamsikmyalapalli@csus.edu>
2019-03-06 23:53:33 -0800

    Add files via upload

--- src/grading/Missing.java
+++ src/grading/Missing.java
@@ -1,17 +1,20 @@
 package grading;
 
 public class Missing {
+	
 	public static final double DEFAULT_MISSING_VALUE = 0.0;
 
 	public static double doubleValue(Double number) {
 		return doubleValue(number, DEFAULT_MISSING_VALUE);
 	}
 
+	// Returns double value
 	public static double doubleValue(Double number, double missingValue) {
 		if (number == null)
 			return missingValue;
 		else
 			return number.doubleValue();
 	}
+	
 }
 
