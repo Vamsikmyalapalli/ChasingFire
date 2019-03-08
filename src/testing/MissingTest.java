fed195bb9d1739ca2a3f612c269d1a493a2d02b6
Vamsi Krishna Myalapalli <vamsikmyalapalli@csus.edu>
2019-03-08 13:26:06 -0800

    Add files via upload

--- src/testing/MissingTest.java
+++ src/testing/MissingTest.java
@@ -7,23 +7,6 @@ import grading.Missing;
 
 public class MissingTest {
 
-//	@Test
-//	public void doubleValue3() 
-//	{
-//		assertEquals("Missing.doubleValue(Double,double,double)", 5.0, Missing.doubleValue(new Double(5.0), 1.0, 1.0), 0.01);
-//		assertEquals("Missing.doubleValue(Double,double,double)", 2.0, Missing.doubleValue(null, 2.0, 1.0), 0.01);
-//		assertEquals("Missing.doubleValue(Double,double,double)", 1.0, Missing.doubleValue(null, 1.0, 1.0), 0.01);
-//		assertEquals("Missing.doubleValue(Double,double,double)", 0.0, Missing.doubleValue(null, 0.0, 1.0), 0.01);
-//		assertEquals("Missing.doubleValue(Double,double,double)", 2.0, Missing.doubleValue(new Double(2.0), 3.0, 1.0), 0.01);
-//		assertEquals("Missing.doubleValue(Double,double,double)", 1.0, Missing.doubleValue(new Double(1.0), 3.0, 1.0), 0.01);
-//		assertEquals("Missing.doubleValue(Double,double,double)", 3.0, Missing.doubleValue(new Double(0.0), 3.0, 1.0), 0.01);
-//
-//		assertEquals("Missing.doubleValue(Double,double,double)", -5.0, Missing.doubleValue(new Double(-5.0),  -9.0, -10.0), 0.01);
-//		assertEquals("Missing.doubleValue(Double,double,double)", -9.0, Missing.doubleValue(new Double(-15.0), -9.0, -10.0), 0.01);
-//		assertEquals("Missing.doubleValue(Double,double,double)", -9.0, Missing.doubleValue(null, -9.0, -10.0), 0.01);
-//		assertEquals("Missing.doubleValue(Double,double,double)", -9.0, Missing.doubleValue(new Double(-15.0), -9.0, -10.0), 0.01);
-//	}
-
 	@Test
 	public void doubleValue2() 
 	{
